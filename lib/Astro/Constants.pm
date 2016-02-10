2b46b89d6723ef2799056b8fe2ad3cc54ee1c917
Boyd Duffee <duffee@cpan.org>
2016-02-10 15:22:47 +0000

    moved all data into data/PhysicalConstants.xml re-wrote xml_to_pm.pl to handle systemless values added write_method_pod to xml_to_pm.pl

--- lib/Astro/Constants.pm
+++ lib/Astro/Constants.pm
@@ -5,108 +5,593 @@ use strict;
 use warnings;
 use base qw/Exporter/;
 
-use constant SPEED_LIGHT => 2.997925e8; 	# mks
 
+=head2 LIGHT_SPEED
 
-our @EXPORT_OK = qw( SPEED_LIGHT 
-	);
+    2.99792458e8	MKS
+    2.99792458e10	CGS
 
+speed of light in a vacuum
 
-=head1 SYNOPSIS
-
-Remove errors in your code by using verified constants that cannot
-be changed.
-
-How easy is it to forget the second equal sign in the equality test?
-compare
-	$constant == $calculated_value;
-with
-	$constant = $calculated_value;
-Both return true, but the second has changed $constant which is not very,
-well, constant.
-Using Astro::Constants,
-	PI = 3;
-is a syntax error and the script doesn't compile meaning that your
-bridges won't fall down because you rounded down.
-
-
-=head1 Thoughts on module design
-
-I've been looking at neil bower's comparison page and I'm faced
-with 2 options functions or variables.  I want speed, so 
-it's either 
-function: constant, Constant::Generate
-or 
-variable: Lexical::Import, Scalar::Constant
-
-Some of these modules also do lists and hashes
-
-=head1 Plans
-
-
-=head2 Astro::Constants
-
-=for :list 
-* unitless constants PI, etc
-* documentation for each constant
-* import all by default (how many are there?)
-
-=head2 Astro::Constants::MKS Astro::Constants::CGS
-
-=for :list 
-* both have use A::C::? qw/long short mass sun cosmology/;
-* nothing is imported by default
-* function for reporting which set of constants are used
-* strict checking to keep from mixing constants
-
-=head2 build script
-
-=for :list 
-* if XML - build modules from XML and astroconst.dat
-* no XML - build modules from shipped astroconst.dat
-* ask if you want functions or variables
-* ask if you want only short or long constants
-* ask where your "bundle" file is
-* use YAML
-* write out astroconst.dat as csv file
-* ask if want MKS or CGS as default
-
-=head1 TODO
-
-=for :list
-
-* write script to "add a constant" to the dat/xml files
-* build script to create "bundles" of constants?
-* write script to verify constant values with NIST using
-	hypermedia concepts and RESTful interfaces.  consider
-	building an API for others to use
-	http://physics.nist.gov/cgi-bin/cuu/CCValue?c
-* create objects that understand units
-* check out old documentation for more TODOs
-* address bug report
-* check issues with CPANTS
-* add a module for interpolated variables for those people
-	who just need to stick the damn things inside strings
-
-=head1 References
-
-=for :list 
-* L<http://physics.nist.gov/cuu/Constants/index.html>
-* L<http://www.bipm.org>
-
-=head1 SEE ALSO
-
-This list also includes modules on Units.
-
-=for :list
-* L<Astro::Constants::MKS>
-* L<Astro::Constants::CGS>
-* L<Astro::SLA>
-* L<Astro::Cosmology>
-* L<Physics::Unit>
-* L<Data::Dimensions>
-* L<Petrophysics::Units>
+This constant can also be accessed through the short name $A_c (deprecated) 
 
 =cut
+
+
+=head2 BOLTZMANN
+
+    1.38064852e-23	MKS
+    1.38064852e-16	CGS
+
+Boltzmann's constant
+
+This constant can also be accessed through the short name $A_k (deprecated) 
+
+=cut
+
+
+=head2 GRAVITATIONAL
+
+    6.67408e-11	MKS
+    6.67408e-8	CGS
+
+universal gravitational constant
+
+This constant can also be accessed through the short name $A_G (deprecated) 
+
+=cut
+
+
+=head2 ELECTRON_VOLT
+
+    1.6021766208e-19	MKS
+    1.6021766208e-12	CGS
+
+electron volt
+
+This constant can also be accessed through the short name $A_eV (deprecated) 
+
+=cut
+
+
+=head2 PLANCK
+
+    6.626070040e-34	MKS
+    6.626070040e-27	CGS
+
+Planck constant
+
+This constant can also be accessed through the short name $A_h (deprecated) 
+
+=cut
+
+
+=head2 HBAR
+
+    1.054571800e-34	MKS
+    1.054571800e-27	CGS
+
+Planck's constant /2pi
+
+This constant can also be accessed through the short name $A_hbar (deprecated) 
+
+=cut
+
+
+=head2 ELECTRON_CHARGE
+
+    1.6021766208e-19	MKS
+    4.8032046729e-10	CGS
+
+electron charge (defined positive)
+
+This constant can also be accessed through the short name $A_e (deprecated) 
+
+=cut
+
+
+=head2 STEFAN_BOLTZMANN
+
+    5.670367e-8	MKS
+    5.670367e-5	CGS
+
+Stefan-Boltzmann constant
+
+This constant can also be accessed through the short name $A_sigma (deprecated) 
+
+=cut
+
+
+=head2 A_RAD
+
+    7.565723e-16	MKS
+    7.565723e-15	CGS
+
+radiation density constant
+
+This constant can also be accessed through the short name $A_arad (deprecated) 
+
+=cut
+
+
+=head2 WIEN
+
+    2.8977729e-3	MKS
+    2.8977729e-1	CGS
+
+Wien wavelength displacement law constant
+
+This constant can also be accessed through the short name $A_Wien (deprecated) 
+
+=cut
+
+
+=head2 ALPHA
+
+    7.2973525664e-3	MKS
+    7.2973525664e-3	CGS
+
+fine structure constant
+
+This constant can also be accessed through the short name $A_alpha (deprecated) 
+
+=cut
+
+
+=head2 VACUUM_IMPEDANCE
+
+    376.730313461
+
+characteristic impedance of vacuum
+
+This constant can also be accessed through the short name $A_Z0 (deprecated) 
+
+=cut
+
+
+=head2 PERMITIV_FREE_SPACE
+
+    8.854187817e-12	MKS
+    1	CGS
+
+permittivity of free space
+
+This constant can also be accessed through the short name $A_eps0 (deprecated) 
+
+=cut
+
+
+=head2 PERMEABL_FREE_SPACE
+
+    1.2566370614e-6	MKS
+    1	CGS
+
+permeability of free space
+
+This constant can also be accessed through the short name $A_mu0 (deprecated) 
+
+=cut
+
+
+=head2 PI
+
+    3.14159265358979324
+
+trig constant pi
+
+This constant can also be accessed through the short name $A_pi (deprecated) 
+
+=cut
+
+
+=head2 EXP
+
+    2.71828182846
+
+base of natural logarithm
+
+This constant can also be accessed through the short name $A_exp (deprecated) 
+
+=cut
+
+
+=head2 ATOMIC_MASS_UNIT
+
+    1.660539040e-27	MKS
+    1.660539040e-24	CGS
+
+atomic mass unit
+
+This constant can also be accessed through the short name $A_amu (deprecated) 
+
+=cut
+
+
+=head2 PARSEC
+
+    3.08567758149e16	MKS
+    3.08567758149e18	CGS
+
+parsec
+
+This constant can also be accessed through the short name $A_pc (deprecated) 
+
+=cut
+
+
+=head2 ASTRONOMICAL_UNIT
+
+    149_597_870_700	MKS
+    1.496e13	CGS
+
+astronomical unit
+
+This constant can also be accessed through the short name $A_AU (deprecated) 
+
+=cut
+
+
+=head2 LIGHT_YEAR
+
+    9_460_730_472_580_800	MKS
+    9.4607304725808e17	CGS
+
+light year
+
+This constant can also be accessed through the short name $A_ly (deprecated) 
+
+=cut
+
+
+=head2 ANGSTROM
+
+    1e-10	MKS
+    1e-8	CGS
+
+Angstrom
+
+This constant can also be accessed through the short name $A_AA (deprecated) 
+
+=cut
+
+
+=head2 JANSKY
+
+    1e-26	MKS
+    1e-23	CGS
+
+Jansky
+
+This constant can also be accessed through the short name $A_Jy (deprecated) 
+
+=cut
+
+
+=head2 AVOGADRO
+
+    6.022140857e23
+
+Avogadro's number
+
+This constant can also be accessed through the short name $A_NA (deprecated) 
+
+=cut
+
+
+=head2 YEAR
+
+    31_557_600
+
+year
+
+This constant can also be accessed through the short name $A_yr (deprecated) 
+
+=cut
+
+
+=head2 SOLAR_MASS
+
+    1.9885e30	MKS
+    1.9885e33	CGS
+
+solar mass
+
+This constant can also be accessed through the short name $A_msun (deprecated) 
+
+=cut
+
+
+=head2 SOLAR_LUMINOSITY
+
+    3.846e26	MKS
+    3.846e33	CGS
+
+solar luminosity
+
+This constant can also be accessed through the short name $A_Lsun (deprecated) 
+
+=cut
+
+
+=head2 RHO_C
+
+    1.8791e-26	MKS
+    1.8791e-29	CGS
+
+critical density /h^2
+
+This constant can also be accessed through the short name $A_rhoc (deprecated) 
+
+=cut
+
+
+=head2 HUBBLE_TIME
+
+    3.0853056e17
+
+Hubble time *h
+
+This constant can also be accessed through the short name $A_tH (deprecated) 
+
+=cut
+
+
+=head2 CMB_TEMPERATURE
+
+    2.725
+
+cosmic microwave background temperature
+
+This constant can also be accessed through the short name $A_TCMB (deprecated) 
+
+=cut
+
+
+=head2 SOLAR_V_MAG
+
+    -26.74
+
+solar V magnitude
+
+This constant can also be accessed through the short name $A_Vsun (deprecated) 
+
+=cut
+
+
+=head2 SOLAR_V_ABS_MAG
+
+    4.83
+
+solar absolute V magnitude
+
+This constant can also be accessed through the short name $A_MVsun (deprecated) 
+
+=cut
+
+
+=head2 SOLAR_RADIUS
+
+    6.96e8	MKS
+    6.96e10	CGS
+
+solar radius
+
+This constant can also be accessed through the short name $A_rsun (deprecated) 
+
+=cut
+
+
+=head2 EARTH_MASS
+
+    5.9726e24	MKS
+    5.9726e27	CGS
+
+mass of Earth
+
+This constant can also be accessed through the short name $A_mearth (deprecated) 
+
+=cut
+
+
+=head2 EARTH_RADIUS
+
+    6.371e6	MKS
+    6.371e8	CGS
+
+radius of Earth
+
+This constant can also be accessed through the short name $A_rearth (deprecated) 
+
+=cut
+
+
+=head2 SOLAR_TEMPERATURE
+
+    5778
+
+surface temperature of sun
+
+This constant can also be accessed through the short name $A_Tsun (deprecated) 
+
+=cut
+
+
+=head2 SOLAR_DENSITY
+
+    1408	MKS
+    1.408	CGS
+
+mean solar density
+
+This constant can also be accessed through the short name $A_dsun (deprecated) 
+
+=cut
+
+
+=head2 EARTH_DENSITY
+
+    5514	MKS
+    5.514	CGS
+
+mean Earth density
+
+This constant can also be accessed through the short name $A_dearth (deprecated) 
+
+=cut
+
+
+=head2 SOLAR_GRAVITY
+
+    274.0	MKS
+    27400	CGS
+
+solar surface gravity
+
+This constant can also be accessed through the short name $A_gsun (deprecated) 
+
+=cut
+
+
+=head2 EARTH_GRAVITY
+
+    9.78	MKS
+    978	CGS
+
+Earth surface gravity
+
+This constant can also be accessed through the short name $A_gearth (deprecated) 
+
+=cut
+
+
+=head2 LUNAR_RADIUS
+
+    1.7381e6	MKS
+    1.7381e8	CGS
+
+lunar radius
+
+This constant can also be accessed through the short name $A_rmoon (deprecated) 
+
+=cut
+
+
+=head2 LUNAR_MASS
+
+    7.342e22	MKS
+    7.342e25	CGS
+
+lunar mass
+
+This constant can also be accessed through the short name $A_mmoon (deprecated) 
+
+=cut
+
+
+=head2 LUNAR_SM_AXIS
+
+    3.844e8	MKS
+    3.844e10	CGS
+
+lunar orbital semi-major axis
+
+This constant can also be accessed through the short name $A_amoon (deprecated) 
+
+=cut
+
+
+=head2 LUNAR_ECCENTRICITY
+
+    0.0549
+
+lunar orbital eccentricity
+
+This constant can also be accessed through the short name $A_emoon (deprecated) 
+
+=cut
+
+
+=head2 THOMSON_XSECTION
+
+    6.6524587158e-29	MKS
+    6.6524587158e-25	CGS
+
+Thomson cross-section
+
+This constant can also be accessed through the short name $A_sigmaT (deprecated) 
+
+=cut
+
+
+=head2 ELECTRON_MASS
+
+    9.10938356e-31	MKS
+    9.10938356e-28	CGS
+
+mass of electron
+
+This constant can also be accessed through the short name $A_me (deprecated) 
+
+=cut
+
+
+=head2 PROTON_MASS
+
+    1.672621898e-27	MKS
+    1.672621898e-24	CGS
+
+mass of proton
+
+This constant can also be accessed through the short name $A_mp (deprecated) 
+
+=cut
+
+
+=head2 NEUTRON_MASS
+
+    1.674927471e-27	MKS
+    1.674927471e-24	CGS
+
+neutron mass
+
+This constant can also be accessed through the short name $A_mn (deprecated) 
+
+=cut
+
+
+=head2 HYDROGEN_MASS
+
+    1.67372e-24
+
+mass of Hydrogen atom
+
+This constant can also be accessed through the short name $A_mH (deprecated) 
+
+=cut
+
+
+=head2 ELECTRON_RADIUS
+
+    2.8179403227e-15	MKS
+    2.8179403227e-13	CGS
+
+classical electron radius
+
+This constant can also be accessed through the short name $A_re (deprecated) 
+
+=cut
+
+
+=head2 BOHR_RADIUS
+
+    5.2917721067e-11	MKS
+    5.2917721067e-9	CGS
+
+Bohr radius
+
+This constant can also be accessed through the short name $A_a0 (deprecated) 
+
+=cut
+
 
 'Perl is my Igor';
--- script/xml_to_pm.pl
+++ script/xml_to_pm.pl
@@ -2,6 +2,8 @@
 #
 # builds the Astro::Constants modules from an xml definition file
 # Boyd Duffee, Dec 2015
+#
+# hard coded to run from top directory and uses only data/PhysicalConstants.xml
 
 use strict;
 use autodie;
@@ -12,18 +14,20 @@ use XML::LibXML;
 
 my (@long, @short, );
 
-my $xml = XML::LibXML->load_xml(location => 'data/output.xml');
+my $xml = XML::LibXML->load_xml(location => 'data/PhysicalConstants.xml');
 
 my $lib = 'lib';	# where is the lib directory
 mkdir "$lib/Astro/Constants" unless -d "$lib/Astro/Constants";
+open my $ac_fh, '>', "$lib/Astro/Constants.pm";
 open my $mks_fh, '>', "$lib/Astro/Constants/MKS.pm";
 open my $cgs_fh, '>', "$lib/Astro/Constants/CGS.pm";
 
+write_module_header($ac_fh, 'Astro::Constants');
 write_module_header($mks_fh, 'Astro::Constants::MKS');
 write_module_header($cgs_fh, 'Astro::Constants::CGS');
 
 for my $constant ( $xml->getElementsByTagName('PhysicalConstant') ) {
-	my ($short_name, $long_name, $mks_value, $cgs_value, ) = undef;
+	my ($short_name, $long_name, $mks_value, $cgs_value, $values, ) = undef;
 
 	for my $name ( $constant->getChildrenByTagName('name') ) {
 		$short_name = $name->textContent() if $name->getAttribute('type') eq 'short';
@@ -32,22 +36,35 @@ for my $constant ( $xml->getElementsByTagName('PhysicalConstant') ) {
 
 	my $description = $constant->getChildrenByTagName('description')->shift()->textContent();
 	for my $value ( $constant->getChildrenByTagName('value') ) {
-		next unless $value->hasAttribute('system');
-
-		$mks_value = $value->textContent() if $value->getAttribute('system') eq 'MKS';
-		$cgs_value = $value->textContent() if $value->getAttribute('system') eq 'CGS';
+		if ( $value->hasAttribute('system') ) {
+			$values->{mks} = $value->textContent() if $value->getAttribute('system') eq 'MKS';
+			$values->{cgs} = $value->textContent() if $value->getAttribute('system') eq 'CGS';
+		}
+		else {
+			$values->{value} = $value->textContent();
+			next;
+		}
 	}
 
-	#say join "\t", $long_name, $short_name, $mks_value, $cgs_value, $description;
 
-	write_method_pod($long_name, $short_name, $description, $mks_value, $cgs_value);
-	write_constant($mks_fh, $mks_value, $long_name, $short_name) if $mks_value;
-	write_constant($cgs_fh, $cgs_value, $long_name, $short_name) if $cgs_value;
+	write_method_pod($ac_fh, $long_name, $short_name, $description, $values);
+	write_constant($mks_fh, ($values->{mks} || $values->{value}), $long_name, $short_name) 
+			if $values->{mks} || $values->{value};
+	write_constant($cgs_fh, ($values->{cgs} || $values->{value}), $long_name, $short_name) 
+			if $values->{cgs} || $values->{value};
+
 
 	push @long, $long_name if $long_name;
 	push @short, $short_name if $short_name;
+		
+	if ( my $alternate = $constant->getChildrenByTagName('alternateName')->shift()->textContent() ) {
+		push @long, $alternate;
+		write_constant($mks_fh, ($values->{mks} || $values->{value}), $alternate) if $values->{mks} || $values->{value};
+		write_constant($cgs_fh, ($values->{cgs} || $values->{value}), $alternate) if $values->{cgs} || $values->{value};
+	}
 }
 
+say $ac_fh "\n'Perl is my Igor';";
 write_module_footer($mks_fh, \@long, \@short);
 write_module_footer($cgs_fh, \@long, \@short);
 
@@ -91,14 +108,29 @@ FOOTER
 }
 
 sub write_method_pod {
-	my ($long_name, $short_name, $description, $mks, $cgs) = @_;
+	my ($fh, $long_name, $short_name, $description, $values) = @_;
 
-	#print "I don't write pod methods yet";
+	my $display;
+	$display .= "    $values->{mks}\tMKS\n" if $values->{mks};
+	$display .= "    $values->{cgs}\tCGS\n" if $values->{cgs};
+	$display ||= "    $values->{value}\n";
+
+	say $fh <<"POD";
+
+=head2 $long_name
+
+$display
+$description
+
+This constant can also be accessed through the short name \$$short_name (deprecated) 
+
+=cut
+POD
 }
 
 sub write_constant {
 	my ($fh, $value, $long_name, $short_name) = @_;
 
 	say $fh "use constant $long_name => $value;";
-	say $fh "use constant $short_name => $value;";
+	say $fh "use constant $short_name = $value;" if $short_name;
 }
